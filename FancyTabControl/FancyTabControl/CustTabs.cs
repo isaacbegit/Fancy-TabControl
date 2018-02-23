39e60e9d72b543f2ca17c059d1c3169918e87eee
Isaac Be <esaaco@gmail.com>
2018-02-23 14:01:52 +0200

    Add files via upload

---
+++ FancyTabControl/FancyTabControl/CustTabs.cs
@@ -0,0 +1,233 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows.Forms;
+namespace LaserDetectionSystem
+{
+    public class MyTab : TabControl
+    {
+        private static List<WeakReference> __ENCList;
+
+        private bool _BlackColor;
+
+        public bool BlackColor
+        {
+            get
+            {
+                return this._BlackColor;
+            }
+            set
+            {
+                this._BlackColor = value;
+                this.Refresh();
+            }
+        }
+
+        [DebuggerNonUserCode]
+        static MyTab()
+        {
+            MyTab.__ENCList = new List<WeakReference>();
+        }
+
+        public MyTab()
+        {
+            List<WeakReference> _ENCList = MyTab.__ENCList;
+            Monitor.Enter(_ENCList);
+            try
+            {
+                MyTab.__ENCList.Add(new WeakReference(this));
+            }
+            finally
+            {
+                Monitor.Exit(_ENCList);
+            }
+            this._BlackColor = true;
+            this.SetStyle(ControlStyles.UserPaint | ControlStyles.ResizeRedraw | ControlStyles.AllPaintingInWmPaint | ControlStyles.DoubleBuffer, true);
+            this.DoubleBuffered = true;
+            this.SizeMode = TabSizeMode.Fixed;
+            this.ItemSize = new Size(44, 136);
+        }
+
+        protected override void CreateHandle()
+        {
+            base.CreateHandle();
+            this.Alignment = TabAlignment.Left;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle tabRect;
+            Rectangle rectangle;
+            Point point;
+            Size size;
+            Point location;
+            Point location1;
+            StringFormat stringFormat;
+            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+            Graphics graphic = Graphics.FromImage(bitmap);
+            try
+            {
+               
+              //  this.SelectedTab.BackColor = Color.FromArgb(50, 50, 50);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Error :" + exception.Message);
+            }
+            graphic.Clear(Color.FromArgb(25, 25, 25));
+            SolidBrush solidBrush = new SolidBrush(Color.FromArgb(44, 55, 72));
+            Size itemSize = this.ItemSize;
+            Rectangle rectangle1 = new Rectangle(0, 0, checked(itemSize.Height + 4), this.Height);
+            graphic.FillRectangle(solidBrush, rectangle1);
+            Pen pen = new Pen(Color.FromArgb(47, 57, 73));
+            Point point1 = new Point(checked(this.Width - 1), 0);
+            Point point2 = new Point(checked(this.Width - 1), checked(this.Height - 1));
+            graphic.DrawLine(pen, point1, point2);
+            Pen pen1 = new Pen(Color.FromArgb(14, 78, 169));
+            itemSize = this.ItemSize;
+            point2 = new Point(checked(itemSize.Height + 1), 0);
+            point1 = new Point(checked(this.Width - 1), 0);
+            graphic.DrawLine(pen1, point2, point1);
+            Pen pen2 = new Pen(Color.FromArgb(14, 78, 169));
+            itemSize = this.ItemSize;
+            point2 = new Point(checked(itemSize.Height + 3), checked(this.Height - 1));
+            point1 = new Point(checked(this.Width - 1), checked(this.Height - 1));
+            graphic.DrawLine(pen2, point2, point1);
+            Pen pen3 = new Pen(Color.FromArgb(14, 78, 169));
+            itemSize = this.ItemSize;
+            point2 = new Point(checked(itemSize.Height + 3), 0);
+            Size itemSize1 = this.ItemSize;
+            point1 = new Point(checked(itemSize1.Height + 3), 999);
+            graphic.DrawLine(pen3, point2, point1);
+            int tabCount = checked(this.TabCount - 1);
+            for (int i = 0; i <= tabCount; i = checked(i + 1))
+            {
+                if (i != this.SelectedIndex)
+                {
+                    rectangle = this.GetTabRect(i);
+                    location1 = rectangle.Location;
+                    int x = checked(location1.X - 2);
+                    location = this.GetTabRect(i).Location;
+                    point = new Point(x, checked(location.Y - 2));
+                    tabRect = this.GetTabRect(i);
+                    int width = checked(tabRect.Width + 3);
+                    Rectangle tabRect1 = this.GetTabRect(i);
+                    size = new Size(width, checked(tabRect1.Height + 1));
+                    Rectangle rectangle2 = new Rectangle(point, size);
+                    graphic.FillRectangle(new SolidBrush(Color.FromArgb(44, 55, 72)), rectangle2);
+                    Pen pen4 = new Pen(Color.FromArgb(25, 91, 183));
+                    location1 = new Point(rectangle2.Right, rectangle2.Top);
+                    location = new Point(rectangle2.Right, rectangle2.Bottom);
+                    graphic.DrawLine(pen4, location1, location);
+                    if (this.ImageList == null)
+                    {
+                        string text = this.TabPages[i].Text;
+                        Font font = new Font(this.Font.FontFamily, this.Font.Size, FontStyle.Bold);
+                        Brush gray = Brushes.Wheat ;
+                        RectangleF rectangleF = rectangle2;
+                        stringFormat = new StringFormat()
+                        {
+                            LineAlignment = StringAlignment.Center,
+                            Alignment = StringAlignment.Center
+                        };
+                        graphic.DrawString(text, font, gray, rectangleF, stringFormat);
+                    }
+                }
+                else
+                {
+                    rectangle1 = this.GetTabRect(i);
+                    point2 = rectangle1.Location;
+                    int num = checked(point2.X - 2);
+                    point1 = this.GetTabRect(i).Location;
+                    Point location2 = new Point(num, checked(point1.Y - 2));
+                    tabRect = this.GetTabRect(i);
+                    int width1 = checked(tabRect.Width + 3);
+                    Rectangle tabRect2 = this.GetTabRect(i);
+                    itemSize1 = new Size(width1, checked(tabRect2.Height - 1));
+                    Rectangle rectangle3 = new Rectangle(location2, itemSize1);
+                    ColorBlend colorBlend = new ColorBlend();
+                    Color[] colorArray = new Color[] { Color.FromArgb(44, 55, 72), Color.FromArgb(44, 55, 72), Color.FromArgb(44, 55, 72) };
+                    Color[] colorArray1 = colorArray;
+                    if (!this.BlackColor)
+                    {
+                        // colorArray1[0] = Color.FromArgb(230, 230, 250);
+                        colorArray1[0] = Color.FromArgb(21, 90, 187);
+                        colorArray1[1] = Color.FromArgb(14, 78, 169);
+                        colorArray1[2] = Color.FromArgb(14, 78, 169);
+                    }
+                    else
+                    {
+                        colorArray1[0] = Color.FromArgb(21, 90, 187);
+                        colorArray1[1] = Color.FromArgb(18, 87, 184);
+                        colorArray1[2] = Color.FromArgb(24, 92, 190);
+                    }
+                    colorBlend.Colors = colorArray1;
+                    colorBlend.Positions = new float[] { 0f, 0.5f, 1f };
+                    LinearGradientBrush linearGradientBrush = new LinearGradientBrush(rectangle3, Color.Red, Color.Red, 90f)
+                    {
+                        InterpolationColors = colorBlend
+                    };
+                    graphic.FillRectangle(linearGradientBrush, rectangle3);
+                    graphic.SmoothingMode = SmoothingMode.HighQuality;
+                    Point[] pointArray = new Point[3];
+                    itemSize1 = this.ItemSize;
+                    int height = checked(itemSize1.Height - 3);
+                    rectangle = this.GetTabRect(i);
+                    location2 = rectangle.Location;
+                    point2 = new Point(height, checked(location2.Y + 20));
+                    pointArray[0] = point2;
+                    itemSize = this.ItemSize;
+                    int height1 = checked(itemSize.Height + 4);
+                    point1 = this.GetTabRect(i).Location;
+                    point = new Point(height1, checked(point1.Y + 14));
+                    pointArray[1] = point;
+                    size = this.ItemSize;
+                    int num1 = checked(size.Height + 4);
+                    location = this.GetTabRect(i).Location;
+                    location1 = new Point(num1, checked(location.Y + 27));
+                    pointArray[2] = location1;
+                    Point[] pointArray1 = pointArray;
+                    graphic.FillPolygon(new SolidBrush(Color.FromArgb(44, 55, 72)), pointArray1);
+                    graphic.DrawPolygon(new Pen(Color.FromArgb(14, 78, 169)), pointArray1);
+                    if (this.ImageList == null)
+                    {
+                        string str = this.TabPages[i].Text;
+                        Font font1 = new Font(this.Font.FontFamily, this.Font.Size, FontStyle.Bold);
+                        SolidBrush solidBrush1 = new SolidBrush(this.TabPages[i].ForeColor);
+                        RectangleF rectangleF1 = rectangle3;
+                        stringFormat = new StringFormat()
+                        {
+                            LineAlignment = StringAlignment.Center,
+                            Alignment = StringAlignment.Center
+                        };
+                        graphic.DrawString(str, font1, solidBrush1, rectangleF1, stringFormat);
+                    }
+                }
+            }
+            Pen pen5 = new Pen(Color.FromArgb(14, 78, 169));
+            size = this.ItemSize;
+            rectangle = new Rectangle(0, 0, checked(size.Height + 3), checked(this.Height - 1));
+            graphic.DrawRectangle(pen5, rectangle);
+            Graphics graphics = e.Graphics;
+            object[] objectValue = new object[] { RuntimeHelpers.GetObjectValue(bitmap.Clone()), 0, 0 };
+           Microsoft.VisualBasic.CompilerServices. NewLateBinding.LateCall(graphics, null, "DrawImage", objectValue, null, null, null, true);
+            graphic.Dispose();
+            bitmap.Dispose();
+        }
+
+        public Brush ToBrush(Color color)
+        {
+            return new SolidBrush(color);
+        }
+
+        public Pen ToPen(Color color)
+        {
+            return new Pen(color);
+        }
+    }
+}
---
+++ FancyTabControl/FancyTabControl/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FancyTabControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myTab1 = new LaserDetectionSystem.MyTab();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.myTab1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myTab1
+            // 
+            this.myTab1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.myTab1.BlackColor = true;
+            this.myTab1.Controls.Add(this.tabPage1);
+            this.myTab1.Controls.Add(this.tabPage2);
+            this.myTab1.Controls.Add(this.tabPage3);
+            this.myTab1.Controls.Add(this.tabPage4);
+            this.myTab1.ItemSize = new System.Drawing.Size(44, 136);
+            this.myTab1.Location = new System.Drawing.Point(37, 24);
+            this.myTab1.Multiline = true;
+            this.myTab1.Name = "myTab1";
+            this.myTab1.SelectedIndex = 0;
+            this.myTab1.Size = new System.Drawing.Size(583, 359);
+            this.myTab1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.myTab1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Location = new System.Drawing.Point(140, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(439, 351);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(140, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(439, 351);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.panel1);
+            this.tabPage3.Location = new System.Drawing.Point(140, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(439, 351);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(79, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 156);
+            this.panel1.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(140, 4);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(439, 351);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 410);
+            this.Controls.Add(this.myTab1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.myTab1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LaserDetectionSystem.MyTab myTab1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
